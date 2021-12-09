ac67d9178ec4d61be994b7262b3ad07852c8bd68
Jesus Pablo Ramirez Villa <58535306+PabloVilla1999@users.noreply.github.com>
2021-12-08 21:57:47 -0800

    Create 2.2.cpp

---
+++ Capitulo-2/2.2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+/**
+ * Capitulo 2 Ejercicio #2.2
+ * Escribe el código ensamblador correspondiente a una estructura if en la que no
+ * exista la rama de else.
+**/
+ 
+ //@Autor Ramirez Villa Jesus Pablo
+ //@GithubAutor PabloVilla1999
+ //@Date 08 Diciembre 2021
+
+using namespace std;
+
+int main()
+{
+    int Num1=12, Num2=14;
+    if(Num2>Num1)
+    {
+        cout<<Num2<<" es mayor que "<<Num1;
+        return 0;
+    }
+}
